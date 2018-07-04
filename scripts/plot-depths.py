1eb88db48432d4101ca7c7db4c0d627fe3a38efe
Johannes Köster <johannes.koester@tu-dortmund.de>
2018-07-05 00:18:35 +0200

    fixes

--- scripts/plot-depths.py
+++ scripts/plot-depths.py
@@ -1,8 +1,17 @@
 import common
 import matplotlib.pyplot as plt
 import pandas as pd
+import seaborn as sns
 
 calls = pd.read_table(snakemake.input[0], header=[0, 1])
-import pdb; pdb.set_trace()
+samples = [name for name in calls.columns.levels[0] if name != "VARIANT"]
+sample_info = calls.loc[:, samples].stack([0, 1]).unstack().reset_index(1, drop=False)
+sample_info = sample_info.rename({"level_1": "sample"}, axis=1)
 
-plt.scatter(x="")
+sample_info = sample_info[sample_info["DP"] > 0]
+sample_info["freq"] = sample_info["AD"] / sample_info["DP"]
+
+
+sns.pairplot(hue="sample", vars=["DP", "freq"], data=sample_info)
+
+plt.savefig(snakemake.output[0])
